57e637d708fcc0a514d8d17417db9b53f4615ae0
yechencheng <yechencheng@gmail.com>
2014-08-23 16:37:16 +0800

    experiment for promotion

---
+++ PromotionSolver.cpp
@@ -0,0 +1,9 @@
+#include <iostrem>
+#include <set>
+#include <list>
+#include <algorithm>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+
+using namespace std;
+
